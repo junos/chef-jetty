16a8455196802532a2a52b13ebc352adfbaf5465
Marco Lopes <marco.paulo.lopes@gmail.com>
2012-09-04 20:33:53 +0100

    Update default jetty version to 8.1.5

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ else
     node.expand!('server')
 end
 
-default["jetty"]["version"]   = "7.6.5.v20120716"
+default["jetty"]["version"]   = "8.1.5.v20120716"
 default["jetty"]["link"]      = "http://download.eclipse.org/jetty/#{node['jetty']['version']}/dist/jetty-distribution-#{node['jetty']['version']}.tar.gz"
 default["jetty"]["checksum"]  = "ae09ecc5e6f7f329a7bb3ae5f939b7ab45c90627b04afb0c22069c53f1868044" # SHA256
 default["jetty"]["directory"] = "/usr/local/src"
